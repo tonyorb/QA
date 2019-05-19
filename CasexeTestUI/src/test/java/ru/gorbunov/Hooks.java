10ec18d8f98c1bf67546dedb1d00e26a1246c449
Антон <@anton>
2019-05-19 20:47:46 +0300

    v1.4

--- CasexeTestUI/src/test/java/ru/gorbunov/Hooks.java
+++ CasexeTestUI/src/test/java/ru/gorbunov/Hooks.java
@@ -13,7 +13,7 @@ public class Hooks {
 
 	@Before
 	public static void setup() {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/hellocucumber/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/ru.gorbunov/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
